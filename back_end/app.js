fad52dc6af2e7a208f8a0e5e8f1ce20a5d22b20f
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-13 14:50:10 -0500

    Use defined routes

--- back_end/app.js
+++ back_end/app.js
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import routes from './src/routes/routes.js';
+
 const app = express();
 
 // Enable CORS
@@ -7,5 +9,8 @@ app.use(cors());
 
 // Parse request body
 app.use(json());
+
+// Use defined endpoints
+app.use(routes);
 
 export default app;
