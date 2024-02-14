fd3fcfdd3c64f89d91aeddd503851896fe1a395b
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-02-14 16:12:22 -0500

    Update database

--- back_end/src/utils/database.js
+++ back_end/src/utils/database.js
@@ -1,10 +1,29 @@
 export const database = [
   {
     id: 1,
-    name: 'Midnight Masquerade',
-    description: 'An evening of intrigue and allure, where hidden identities and masked secrets add an air of mystique to the festivities',
+    name: 'Midnight Concert',
+    description: 'Just the best concert of your life',
     date: '20-02-2024',
     capacity: 5,
+    picture: 'https://i.postimg.cc/Pq5MXZMR/muneeb-syed-4-M8u-If-PEZw-unsplash.jpg',
+    registrations: []
+  },
+  {
+    id: 2,
+    name: 'Sunset Yoga Session',
+    description: 'Relax your mind and body with a peaceful yoga session at sunset',
+    date: '25-02-2024',
+    capacity: 10,
+    picture: 'https://i.postimg.cc/Px2zvMtD/jared-rice-NTy-Bbu66-SI-unsplash.jpg',
+    registrations: []
+  },
+  {
+    id: 3,
+    name: 'Adventure Trek: Mystic Mountain Expedition',
+    description: 'Embark on an exhilarating journey through the mystical mountains, exploring hidden caves and breathtaking vistas',
+    date: '15-04-2024',
+    capacity: 10,
+    picture: 'https://i.postimg.cc/zX3GCR8R/pietro-de-grandi-Q5d-Mq3c-Kqec-unsplash.jpg',
     registrations: []
   }
 ];
